9a4d48bc8e4aa16b8f0ab3120a6cf797fd20c609
Gary <garycxjk@gmail.com>
2022-02-14 22:50:06 +0100

    Added design loading

--- src/main/managers/designs-manager.js
+++ src/main/managers/designs-manager.js
@@ -1,54 +1,59 @@
 import deepmerge from "deepmerge";
 import { ipcMain } from "electron";
 import traverse from "../../helpers/traverse";
+import { fetchEntities, loadEntity, queueEntity } from "./file-manager";
 
-const designs = {
-  default: {
-    panels: {
-      layout: [
-        {
-          type: 'image',
-          size: ['panel', 'preview'],
-        },
-        {
-          type: 'label',
-          display: 'image',
-        },
-      ],
-      border: {
-        width: '0.1em',
+const designs = {};
+const designQueue = [];
+const waiting = {};
+
+let queueIsRunning = null;
+
+const defaultDesign = {
+  panels: {
+    layout: [
+      {
+        type: 'image',
+        size: ['panel', 'preview'],
       },
-      gap: '0.25em',
-      margin: '1.5em',
-      image: {
-        filters: [
-          {
-            type: 'drop-shadow',
-            value: {
-              x: '0.5em',
-              y: '0.5em',
-              radius: '0.2em',
-              color: '#222',
-            },
-          }
-        ],
+      {
+        type: 'label',
+        display: 'image',
       },
+    ],
+    border: {
+      width: '0.1em',
     },
-    preview: {
-      image: {
-        filters: [
-          {
-            type: 'drop-shadow',
-            value: {
-              x: '0.5em',
-              y: '0.5em',
-              radius: '0.2em',
-              color: '#222',
-            },
-          }
-        ],
-      },
-    }
+    gap: '0.25em',
+    margin: '1.5em',
+    image: {
+      filters: [
+        {
+          type: 'drop-shadow',
+          value: {
+            x: '0.5em',
+            y: '0.5em',
+            radius: '0.2em',
+            color: '#222',
+          },
+        }
+      ],
+    },
+  },
+  preview: {
+    image: {
+      filters: [
+        {
+          type: 'drop-shadow',
+          value: {
+            x: '0.5em',
+            y: '0.5em',
+            radius: '0.2em',
+            color: '#222',
+          },
+        }
+      ],
+    },
   },
 };
 const sizes = {
@@ -83,15 +88,47 @@ export const getSize = async (type, sizeId, designId = null) => {
   return null;
 }
 
-export const getDesign = async (designId = 'default') => {
-  const sizeSegments = designId.split('>');
+export const getDesign = async (designId = null) => {
 
-  let design = traverse(sizeSegments, designs);
-  if (designId !== 'default') {
-    design = deepmerge(getDesign(), design);
+  let design = deepmerge({}, defaultDesign);
+  if (designId) {
+    const sizeSegments = designId.split('>');
+    design = traverse(sizeSegments, designs);
+    if (!design) {
+      const designGroup = sizeSegments.slice(0, 2).join('>');
+      queueEntity(designQueue, waiting, designGroup);
+    }
+    design = deepmerge(getDesign(), defaultDesign);
   }
 
   return design;
+}
+
+export const fetchDesigns = async (packFolder) => {
+  return await fetchEntities('designs', packFolder);
+}
+
+const loadDesign = async (designId) => {
+  return loadEntity('designs', waiting, designs, designId);
+}
+
+const runDesignQueue = async () => {
+  if (queueIsRunning) {
+    return;
+  }
+  queueIsRunning = createWaiter();
+  while (designQueue.length) {
+    const designId = designQueue.shift();
+    await loadDesign(designId);
+  }
+  queueIsRunning.resolve();
+  queueIsRunning = null;
+}
+
+export const queueDesign = (designId) => {
+  queueEntity(designQueue, waiting, designId);
+
+  runDesignQueue();
 }
 
 ipcMain.handle('designs:get', (_event, designId) => getDesign(designId));
