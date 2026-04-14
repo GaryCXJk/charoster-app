93029068453ab6dc57a68aa5ede7f1212e949ffa
Gary Kertopermono <garycxjk@gmail.com>
2026-04-14 16:20:58 +0200

    feat(definitions): added works and appearances definitions with fields

--- src/global/definitions.js
+++ src/global/definitions.js
@@ -14,3 +14,48 @@ export const franchises = {
     }
   }
 };
+
+export const works = {
+  id: "work",
+  name: "Work",
+  folder: "works",
+  merge: "ignore",
+  discover: "works",
+  list: true,
+  fields: {
+    name: "string",
+    description: "string",
+    franchise: {
+      name: "Franchise",
+      type: "definition",
+      value: "franchise",
+      multi: false,
+    },
+    type: {
+      name: "Type",
+      type: "enum",
+      options: ["movie", "tv_show", "comic_book", "video_game", "other"],
+    },
+    startDate: "date",
+    endDate: "date"
+  }
+};
+
+export const appearances = {
+  id: "appearance",
+  name: "Appearance",
+  properties: {
+    work: {
+      name: "Work",
+      type: "definition",
+      value: "work",
+      multi: false,
+    },
+    kind: {
+      name: "Kind",
+      type: "enum",
+      multi: true,
+      options: ["first", "last", "cameo"]
+    }
+  },
+};
